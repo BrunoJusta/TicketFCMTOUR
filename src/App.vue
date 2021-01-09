26449fa51ca973855977ae4db5d619f1f64213dc
BrunoJusta <brunomajusta@gmail.com>
2021-01-09 01:06:32 +0000

    commit

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <template>
   <div id="app">
-    <div id="nav">
+   <!--  <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
-    </div>
+    </div> -->
     <router-view/>
   </div>
 </template>
@@ -15,9 +15,10 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+
 }
 
-#nav {
+/* #nav {
   padding: 30px;
 }
 
@@ -28,5 +29,5 @@
 
 #nav a.router-link-exact-active {
   color: #42b983;
-}
+} */
 </style>
